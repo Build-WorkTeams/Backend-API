1c45983fc1eeeb03f3c7d217063d44121fa707b3
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2024-03-14 16:55:53 +0100

    Implement Static collection

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ set -o errexit
 
 pip install -r requirements.txt
 
-# # Convert static asset files
-# python manage.py collectstatic --no-input
+# Convert static asset files
+python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
